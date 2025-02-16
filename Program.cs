00f97c3e97e7dbbc5fd34516e8cb661a365e0349
stephenChinag <stephenchinag@gmail.com>
2025-02-16 23:57:37 +0100

    first

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void CalculateSum(int a, int b  )
+    {
+        int sum = a + b; // Must assign a value
+        Console.WriteLine(sum);
+    }
+
+    static void Main()
+    {
+   
+        CalculateSum(3, 7  );
+        // Console.WriteLine(result); // Output: 10
+    }
+}
