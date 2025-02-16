c5dd5a6d0992955017861f0152b5e98759e68502
stephenChinag <stephenchinag@gmail.com>
2025-02-17 00:33:09 +0100

    class librar

---
+++ MyLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace MyLibrary;
+
+public class Class1
+{
+public static int Add(int a , int b) {
+return a +b;
+}
+
+public static int Sub (int a , int b) {
+    return a - b;
+}
+}
--- Program.cs
+++ Program.cs
@@ -1,17 +1,24 @@
 ﻿using System;
-
+using MyLibrary;
 class Program
 {
-    static void CalculateSum(int a, int b  )
-    {
-        int sum = a + b; // Must assign a value
-        Console.WriteLine(sum);
-    }
+  static void PrintNumbers (params int[] numbers){
 
-    static void Main()
-    {
-   
-        CalculateSum(3, 7  );
-        // Console.WriteLine(result); // Output: 10
+    foreach (int num in numbers) {
+        Console.Write(num + " all ");
     }
+  }
+
+
+  static void Main () 
+
+  {
+
+    int result = Class1.Add(1,3);
+    Console.WriteLine("Add = " +result);
+
+    int subResult = Class1.Sub(3, 2);
+    Console.WriteLine("sub = "+subResult);
+    PrintNumbers (1,2,3,3,4);
+  }
 }
